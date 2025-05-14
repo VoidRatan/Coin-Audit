8f25e865613221ae9d3bea1ab7aa87878a26c923
Prasun Ratan <prasunratan@gmail.com>
2025-05-14 12:19:10 +0530

    Create Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @ 2025, CoinAudit - All Right Reserved.</p>
+      <p>Developed by voidRatan</p>
+    </div>
+  )
+}
+
+export default Footer
